e456c085f560a6f2163c582d84f4df589a0c971f
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-05-07 23:40:27 +0530

    Solved 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(obj) {
+  let titles = [];
 
+  for(key in obj) {
+    titles.push(obj[key].title);
+  }
+
+  return titles;
 };
 
 // Do not edit below this line
