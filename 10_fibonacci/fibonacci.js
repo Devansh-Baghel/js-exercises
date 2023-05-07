b80677fd3e6779dcfa7743d2c760ee7fcb6772f0
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-05-07 23:37:38 +0530

    Solved 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  let sequence = [];
+  let number1 = 1;
+  let number2 = 1;
+  let nexNumber;
 
+  for (let i = 0; i < index; i++){
+    sequence.push(number1);
+    nexNumber = number1 + number2;
+    number1 = number2;
+    number2 = nexNumber;
+  }
+  return sequence[index-1]
 };
 
 // Do not edit below this line
