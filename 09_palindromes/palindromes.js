226e7ee0e4c3fed2bcb7bcb1d54359c889e88d90
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-05-07 23:05:16 +0530

    Solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let result = "";
 
+  // Loop over the string in reverse and append each character
+  for(let i = str.length - 1; i >= 0; i--){
+    result += str[i];
+  }
+
+  if (result === str) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
