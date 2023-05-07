8379724ea660df6466c7a96666fae2703c9a8888
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-05-07 23:59:03 +0530

    Solved 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+  let oldestAgeCounter = 0;
+  let oldestPersonIndex = 0;
 
+  for (key in obj) {
+    if ((obj[key].yearOfDeath - obj[key].yearOfBirth) > oldestAgeCounter) {
+      oldestAgeCounter = obj[key].yearOfDeath - obj[key].yearOfBirth;
+      oldestPersonIndex = key;
+    }
+  }
+
+  return obj[oldestPersonIndex];
 };
 
 // Do not edit below this line
